d08810f28dbaccad85c8ba1600c835ef6711accf
R.M.SRIAHRAN <134301913+RMSriharan@users.noreply.github.com>
2024-05-25 12:22:39 +0530

    Time: 39 ms (26.33%) | Memory: 16.6 MB (11.90%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordset=set(wordDict)
+        @cache
+        def helper(t):
+            combos=[]
+            if not t:
+                return [""]
+            for i,_ in enumerate(t):
+                w=t[:i+1]
+                if w in wordset:
+                    combos.extend([f'{w} {sentence}'if sentence else w
+                    for sentence in helper(t[i+1:]) 
+                    ])
+            return combos
+        return helper(s)
